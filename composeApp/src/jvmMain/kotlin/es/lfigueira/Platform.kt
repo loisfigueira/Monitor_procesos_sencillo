4af623d5682c7a9a598117e555a0ac5f723cf9d2
lfigueira <jfigueiralijo@gmail.com>
2025-11-02 21:19:33 +0100

    Subida inicial del proyecto

---
+++ composeApp/src/jvmMain/kotlin/es/lfigueira/Platform.kt
@@ -0,0 +1,7 @@
+package es.lfigueira
+
+class JVMPlatform {
+    val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+fun getPlatform() = JVMPlatform()
---
+++ composeApp/src/jvmMain/kotlin/es/lfigueira/main.kt
@@ -0,0 +1,13 @@
+package es.lfigueira
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Monitor_procesos_sencillo",
+    ) {
+        App()
+    }
+}
