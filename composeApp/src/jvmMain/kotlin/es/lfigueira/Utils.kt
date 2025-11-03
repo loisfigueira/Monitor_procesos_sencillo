9f046118d0b2700a1beb54014b97ceb88c3ca455
lfigueira <jfigueiralijo@gmail.com>
2025-11-03 18:00:07 +0100

    Comprobación del sistema operativo

--- composeApp/src/jvmMain/kotlin/es/lfigueira/Utils.kt
+++ composeApp/src/jvmMain/kotlin/es/lfigueira/Utils.kt
@@ -1,1 +1,15 @@
 package es.lfigueira
+
+enum class OperatingSystem {
+    WINDOWS, LINUX, MAC, UNKNOWN
+}
+
+fun detectOS(): OperatingSystem {
+    val osName = System.getProperty("os.name").lowercase()
+    return  when {
+        osName.contains("windows") -> OperatingSystem.WINDOWS
+        osName.contains("linux") -> OperatingSystem.LINUX
+        osName.contains("mac") || osName.contains("darwin") -> OperatingSystem.MAC
+        else -> OperatingSystem.UNKNOWN
+    }
+}
--- composeApp/src/jvmMain/kotlin/es/lfigueira/main.kt
+++ composeApp/src/jvmMain/kotlin/es/lfigueira/main.kt
@@ -2,12 +2,13 @@ package es.lfigueira
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.material3.Text
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Monitor_procesos_sencillo",
     ) {
-
+        Text("Sistema operativo detectado: ${detectOS()}")
     }
 }
